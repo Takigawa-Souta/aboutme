141bf2e5b3cd2cc6d0fca526635ac98c1b2a77dd
noasnr0406 <167691342+noasnr0406@users.noreply.github.com>
2025-06-14 11:24:40 +0900

    変更、header

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -2,13 +2,17 @@ import styles from './Header.module.css'
 
 interface HeaderProps {
   title?: string
+  subtitle?:string
 }
 
-function Header({ title = "About Me" }: HeaderProps) {
+function Header({ title = "瀧川 颯太",subtitle="スーパーITエンジニア専攻所属"}:
+HeaderProps) {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}
+        </h2>
       </div>
     </header>
   )
